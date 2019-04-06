13eee1f5fba4b7e335a4571b2e52b980102f6410
unknown <Digital@DH-A8-23.dhbrasil.digitalhouse.com>
2019-04-05 21:10:21 -0300

    Initial commit

---
+++ Filho.java
@@ -0,0 +1,38 @@
+package br.com.digitalhouse;
+
+public class Filho {
+
+    private String mae;
+    private String pai;
+    private String nome;
+
+    public Filho(String nomeMae, String nomePai, String nomeFilho){
+        mae = nomeMae;
+        pai = nomePai;
+        nome = nomeFilho;
+    }
+
+    public String getMae() {
+        return mae;
+    }
+
+    public void setMae(String mae) {
+        this.mae = mae;
+    }
+
+    public String getPai() {
+        return pai;
+    }
+
+    public void setPai(String pai) {
+        this.pai = pai;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,33 @@
+package br.com.digitalhouse;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Filho filho1 = new Filho("Maria","Jorge","Jose");
+        Filho filho2 = new Filho("Maria","Fernando","Ana");
+
+        System.out.println(saoMeioIrmaos(filho1,filho2));
+    }
+
+    public static String maeDe(Filho filho){
+        return filho.getMae();
+    }
+
+    public static String paiDe(Filho filho){
+        return filho.getPai();
+    }
+
+    public static boolean temAmesmaMae(Filho filho1,Filho filho2){
+        return maeDe(filho1) == maeDe(filho2);
+    }
+
+    public static boolean temOmesmoPai(Filho filho1,Filho filho2){
+        return paiDe(filho1) == paiDe(filho2);
+    }
+
+    public static boolean saoMeioIrmaos(Filho filho1,Filho filho2){
+        return temAmesmaMae(filho1,filho2) && !temOmesmoPai(filho1,filho2)
+                || !temAmesmaMae(filho1,filho2) && temOmesmoPai(filho1,filho2) ;
+    }
+}
